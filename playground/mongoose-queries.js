7885d112e941ab1f15833a56efea9292e80d4b99
ben <benpou2012@gmail.com>
2017-08-21 19:12:39 -0700

    all change

--- playground/mongoose-queries.js
+++ playground/mongoose-queries.js
@@ -1,3 +1,18 @@
 const {mongoose} = require('./../server/db/mongoose');
-const {Todp} = require('./../server/models/todo');
+const {Todo} = require('./../server/models/todo');
+
+
+var id = '5998a41d637e6b826cc78a3e';
+
+Todo.find({
+    _id: id
+)}.then((todos) => {
+    console.log('Todos', todos);
+});
+
+Todo.findOne({
+    _id: id
+)}.then((todos) => {
+    console.log('Todo', todo);
+});
 
